0e6e590b0c6fd026007ecdd24a3d513b0658e99e
Tom <xiaolong@cse.tamu.edu>
2019-09-24 13:36:49 -0400

    Export local storage

--- projects/polpware/tinymce-util/src/public-api.ts
+++ projects/polpware/tinymce-util/src/public-api.ts
@@ -7,18 +7,22 @@ export * from './lib/util/ArrUtils';
 export * from './lib/util/LazyEvaluator';
 export * from './lib/util/Predicate';
 export * from './lib/util/Private';
+export * from './lib/api/Env';
 export * from './lib/api/util/Class';
-export * from './lib/api/util/I18n';
-export * from './lib/api/util/Color';
+export * from './lib/api/util/Delay';
+// export * from './lib/api/util/FakeStorage';
 export * from './lib/api/util/JSON';
 export * from './lib/api/util/JSONRequest';
+export * from './lib/api/util/Observable';
+export * from './lib/api/util/Tools';
+export * from './lib/api/util/Color';
+export * from './lib/api/util/I18n';
 export * from './lib/api/util/EventDispatcher';
 export * from './lib/api/util/Promise';
-export * from './lib/api/util/Tools';
 export * from './lib/api/util/URI';
 export * from './lib/api/util/XHR';
-export * from './lib/api/Env';
-export * from './lib/api/util/Observable';
+export * from './lib/api/util/LocalStorage';
+
 
 
 
